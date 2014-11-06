636649ee5a62c660e8508e0722a76568078a5fb8
dimention <djdalgis@gmail.com>
2014-11-06 17:04:57 +0200

    fix recipe service #5

--- src/NfqAkademija/RecipeBundle/Services/IngredientServices.php
+++ src/NfqAkademija/RecipeBundle/Services/IngredientServices.php
@@ -10,24 +10,24 @@ use NfqAkademija\RecipeBundle\Entity\Ingredient;
 
 class IngredientServices
 {
-	protected $em;
+    protected $em;
 
     public function __construct(EntityManager $em)
     {
         $this->em = $em;
     }
 
-	public function getIngredient($term = null)
-	{
-		$ingredients = $this->em->createQuery('SELECT i.id, i.name FROM RecipeBundle:Ingredient i WHERE i.name like :searchterm ORDER BY i.id ASC')->setParameter('searchterm', '%'.$term.'%')->getResult();
-		return $ingredients; 
-	}
+    public function getIngredient($term = null)
+    {
+        $ingredients = $this->em->createQuery('SELECT i.id, i.name FROM RecipeBundle:Ingredient i WHERE i.name like :searchterm ORDER BY i.id ASC')->setParameter('searchterm', '%'.$term.'%')->getResult();
+        return $ingredients;
+    }
 
-	public function getRecipes()
-	{
-		$repository = $this->em->getRepository('RecipeBundle:Recipe');
-		$recipes = $repository->findAll();
-		foreach ($recipes as $recipe) {
+    public function getRecipes()
+    {
+        $repository = $this->em->getRepository('RecipeBundle:Recipe');
+        $recipes = $repository->findAll();
+        foreach ($recipes as $recipe) {
             $uno = [];
             $uno['id'] = $recipe->getId();
             $uno['name'] = $recipe->getName();
@@ -36,7 +36,11 @@ class IngredientServices
             $recipe_ingredients = $recipe->getIngredients();
             $ingredient_array = [];
             foreach ($recipe_ingredients as $ingredient) {
-                $ingredient_array[]= $ingredient->getIngredient()->getName();
+                $iuno =[];
+                $iuno['name']= $ingredient->getIngredient()->getName();
+                $iuno['quantity']= $ingredient->getQuantity();
+                $iuno['unit']= $ingredient->getUnit()->getShort();
+                $ingredient_array[] = $iuno;
             }
             $uno['ingredients'] = $ingredient_array;
             $images_array = [];
@@ -45,29 +49,29 @@ class IngredientServices
             }
             $uno['images'] = $images_array;
             $recipess[] = $uno;
-		}
-		//var_dump($recipess);die();
-		return $recipess;
-		//var_dump($recipe); die();    
-	}
-	public function getRecipe($id)
-	{
-		$repository = $this->em->getRepository('RecipeBundle:Recipe');
-		$recipe = $repository->findOneById($id);
-				$recipe1['id'] = $recipe->getId();
-				$recipe1['name'] = $recipe->getName();
-				$recipe1['instructions'] = $recipe->getInstructions();
-				$recipe_images = $recipe->getImages();
-				$recipe_ingredients = $recipe->getIngredients();
-				foreach ($recipe_ingredients as $ingredient) {
-					$ingredient_array[]= $ingredient->getIngredient()->getName();
-				}
-				$recipe1['ingredients'] = $ingredient_array;
-				foreach ($recipe_images as $image) {
-					$images_array[]= $image->getWebPath();
-				}
-				$recipe1['images'] = $images_array;
-		return $recipe1;    
-	}
-    
+        }
+        //var_dump($recipess);die();
+        return $recipess;
+        //var_dump($recipe); die();
+    }
+    public function getRecipe($id)
+    {
+        $repository = $this->em->getRepository('RecipeBundle:Recipe');
+        $recipe = $repository->findOneById($id);
+        $recipe1['id'] = $recipe->getId();
+        $recipe1['name'] = $recipe->getName();
+        $recipe1['instructions'] = $recipe->getInstructions();
+        $recipe_images = $recipe->getImages();
+        $recipe_ingredients = $recipe->getIngredients();
+        foreach ($recipe_ingredients as $ingredient) {
+            $ingredient_array[]= $ingredient->getIngredient()->getName();
+        }
+        $recipe1['ingredients'] = $ingredient_array;
+        foreach ($recipe_images as $image) {
+            $images_array[]= $image->getWebPath();
+        }
+        $recipe1['images'] = $images_array;
+        return $recipe1;
+    }
+
 }
