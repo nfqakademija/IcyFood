7af693c2bf3c880e5ccd334f86fee4dc9e66d30e
dimention <djdalgis@gmail.com>
2014-11-21 02:39:09 +0200

    fixed menu items active selection

--- src/NfqAkademija/RecipeBundle/Resources/public/js/app/controllers/nav-controller.js
+++ src/NfqAkademija/RecipeBundle/Resources/public/js/app/controllers/nav-controller.js
@@ -1,5 +1,5 @@
 recipeApp.controller('navController', function($scope, $location) {
     $scope.isActive = function (viewLocation) {
-        return viewLocation === $location.url();
+        return viewLocation === $location.url() ? "active" : "";
     };
 });
